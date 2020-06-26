83a2a525c4c015d8ca86ff08290b23b7c6c17a1a
Sarah Addo <saddo@google.com>
2020-06-26 15:30:07 +0000

    fix indentation

--- src/main/webapp/index_script.js
+++ src/main/webapp/index_script.js
@@ -11,43 +11,43 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
- 
+
 document.addEventListener("DOMContentLoaded", async function() {
-    let organizations = [{
-        "name" : "Organization A",
-    },
-    {
-        "name" : "Organization B",
-    },
-    {
-        "name" : "Organization C",
-    },
-    {
-        "name" : "Organization D",
-    },
-    {
-        "name" : "Organization E",
-    },
-    {
-        "name" : "Organization F"
-    }];
-    const organizationArea = document.getElementById('organization-list');
-    organizations.forEach((organization) => {
-      organizationArea.appendChild(createOrganization(organization));
-    });
+  let organizations = [{
+    "name": "Organization A",
+  },
+  {
+    "name": "Organization B",
+  },
+  {
+    "name": "Organization C",
+  },
+  {
+    "name": "Organization D",
+  },
+  {
+    "name": "Organization E",
+  },
+  {
+    "name": "Organization F"
+  }];
+  const organizationArea = document.getElementById('organization-list');
+  organizations.forEach((organization) => {
+    organizationArea.appendChild(createOrganization(organization));
   });
+});
 
 function createOrganization(organization) {
   const organizationElement = document.createElement("div");
   organizationElement.classList.add("organization");
- 
+
   const organizationNameElement = document.createElement('div');
   organizationNameElement.classList.add("organization-name");
   organizationNameElement.textContent = organization.name;
- 
+
   organizationElement.addEventListener('click', () => {
   });
- 
+
   organizationElement.appendChild(organizationNameElement);
   return organizationElement;
 }
