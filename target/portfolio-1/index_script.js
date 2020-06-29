c2476bc61fb179adb941cc4b935146664b8d518f
Andrew <masek@google.com>
2020-06-29 14:11:52 +0000

    Code tweaks

--- target/portfolio-1/index_script.js
+++ target/portfolio-1/index_script.js
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+<<<<<<< HEAD
 
 document.addEventListener("DOMContentLoaded", async function() {
   const searchArea = new SearchArea(document.getElementById('search-area'));
@@ -137,4 +138,46 @@ class SearchArea {
     popupElement.appendChild(popupAddressElement);
     return popupElement;
   }
+=======
+ 
+document.addEventListener("DOMContentLoaded", async function() {
+    let organizations = [{
+        "name" : "Organization A",
+    },
+    {
+        "name" : "Organization B",
+    },
+    {
+        "name" : "Organization C",
+    },
+    {
+        "name" : "Organization D",
+    },
+    {
+        "name" : "Organization E",
+    },
+    {
+        "name" : "Organization F"
+    }];
+    const organizationArea = document.getElementById('organization-list');
+    organizations.forEach((organization) => {
+      organizationArea.appendChild(createOrganization(organization));
+    });
+  });
+
+function createOrganization(organization) {
+  const organizationElement = document.createElement("div");
+  organizationElement.classList.add("organization");
+ 
+  const organizationNameElement = document.createElement('div');
+  organizationNameElement.classList.add("organization-name");
+  organizationNameElement.textContent = organization.name;
+ 
+  organizationElement.addEventListener('click', () => {
+   console.log("hello");
+  });
+ 
+  organizationElement.appendChild(organizationNameElement);
+  return organizationElement;
+>>>>>>> Code tweaks
 }
