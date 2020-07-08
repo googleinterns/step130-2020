1d2d744759399102ac08598eadb073b539318f5a
Sarah Addo <saddo@google.com>
2020-07-08 20:11:31 +0000

    create organization updater class

--- src/main/java/com/google/sps/data/Organization.java
+++ src/main/java/com/google/sps/data/Organization.java
@@ -15,12 +15,7 @@
 package com.google.sps.data;
 
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Arrays;
 import com.google.appengine.api.datastore.Entity;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 
 public final class Organization {
 
@@ -51,90 +46,5 @@ public final class Organization {
      this.isApproved = (boolean) entity.getProperty("isApproved");
      this.urlLink = (String) entity.getProperty("orgUrl");
      this.moderators = (ArrayList) entity.getProperty("moderatorList");
-  }
-
-  public void updateOrganization(HttpServletRequest request, boolean isMaintainer, boolean isModerator) throws IllegalArgumentException{
-    Map<String, String> properties = new HashMap<String,String>();
-
-    // Format is Form Name, Entity Property 
-    properties.put("org-name", "orgName");
-    properties.put("org-email", "orgEmail");
-    properties.put("org-street-address", "orgStreetAddress");
-    properties.put("org-phone-num", "orgPhoneNum");
-    properties.put("org-url", "orgUrl");
-    properties.put("org-description","orgDescription");
-    properties.put("approval", "isApproved");
-    properties.put("moderator-list", "moderatorList");
-
-    for(Map.Entry<String, String> entry : properties.entrySet()) {
-      String formKey = entry.getKey();
-      String propertyKey = entry.getValue();
-      String formValue = "";
-
-      // will only get approval param is a maintainer sent the request
-      if(propertyKey.equals("isApproved") && !isMaintainer) {
-        continue;
-      }
-
-      // will only get moderatorList param if request is sent by a moderator or maintainer
-      if(propertyKey.equals("moderatorList") && !isModerator && !isMaintainer) {
-        continue;
-      }
-
-      // setting organization description is optional
-      if(!propertyKey.equals("orgDescription")) {
-        try {
-          formValue = getParameterOrThrow(request, formKey);
-        } catch(IllegalArgumentException err) {
-          throw new IllegalArgumentException();
-        }
-      } else {
-        formValue = request.getParameter(formKey);
-      }
-      setOrganizationProperty(propertyKey, formValue);
-    }
-  }
-
-  public void commitOrganization(Entity entity) {
-    entity.setProperty("orgName", name);
-    entity.setProperty("orgEmail", email);
-    entity.setProperty("orgStreetAddress", address);
-    entity.setProperty("orgDescription", description);
-    entity.setProperty("orgPhoneNum", phoneNum);
-    entity.setProperty("isApproved", isApproved);
-    entity.setProperty("orgUrl", urlLink);
-    entity.setProperty("moderatorList", moderators);
-  }
-
-  private String getParameterOrThrow(HttpServletRequest request, String formKey) {
-    String result = request.getParameter(formKey);
-    if(result.isEmpty()) {
-        throw new IllegalArgumentException("Form value cannot be null");
-    }
-    return result;
-  }
-
-  private void setOrganizationProperty(String propertyKey, String formValue) {
-    if(propertyKey.equals("moderatorList")) {
-      this.moderators = new ArrayList<String>(Arrays.asList(formValue.split("\\s*,\\s*")));
-    } else if(propertyKey.equals("isApproved")) {
-      if(formValue.equals("approved")) {
-        this.isApproved = true;
-      } else if(formValue.equals("notApproved")) {
-          this.isApproved = false;
-        }
-    } else if(propertyKey.equals("orgName")) {
-      this.name = formValue;
-    } else if(propertyKey.equals("orgEmail")) {
-      this.email = formValue;
-    } else if(propertyKey.equals("orgStreetAddress")) {
-      this.address = formValue;
-    } else if(propertyKey.equals("orgDescription")) {
-      this.description = formValue;
-    } else if(propertyKey.equals("orgPhoneNum")) {
-      this.phoneNum = formValue;
-    } else if(propertyKey.equals("orgUrl")) {
-      this.urlLink = formValue;
-    }
   }
 }
---
+++ src/main/java/com/google/sps/data/OrganizationUpdater.java
@@ -0,0 +1,108 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.data;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Arrays;
+import com.google.appengine.api.datastore.Entity;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+public final class OrganizationUpdater {
+
+  private Entity entity;
+
+  public OrganizationUpdater(Entity entity) {
+     this.entity = entity;
+  }
+
+  public Entity getEntity() {
+      return this.entity;
+  }
+
+  public void updateOrganization(HttpServletRequest request, boolean isMaintainer, boolean isModerator) throws IllegalArgumentException{
+    Set<String> requiresMaintainer = new HashSet<String>();
+    Set<String> requiresModerator = new HashSet<String>();
+    Map<String, String> properties = new HashMap<String,String>();
+
+    requiresMaintainer.add("isApproved");
+    requiresModerator.add("moderatorList");
+
+    // Format is Form Name, Entity Property 
+    properties.put("org-name", "orgName");
+    properties.put("org-email", "orgEmail");
+    properties.put("org-street-address", "orgStreetAddress");
+    properties.put("org-phone-num", "orgPhoneNum");
+    properties.put("org-url", "orgUrl");
+    properties.put("org-description","orgDescription");
+    properties.put("approval", "isApproved");
+    properties.put("moderator-list", "moderatorList");
+
+    for(Map.Entry<String, String> entry : properties.entrySet()) {
+      String formKey = entry.getKey();
+      String propertyKey = entry.getValue();
+      String formValue = "";
+
+      // will only get approval param is a maintainer sent the request
+      if(requiresMaintainer.contains(propertyKey) && !isMaintainer) {
+        continue;
+      }
+
+      // will only get moderatorList param if request is sent by a moderator or maintainer
+      if(requiresModerator.contains(propertyKey) && !isModerator && !isMaintainer) {
+        continue;
+      }
+
+      // setting organization description is optional
+      if(!propertyKey.equals("orgDescription")) {
+        try {
+          formValue = getParameterOrThrow(request, formKey);
+        } catch(IllegalArgumentException err) {
+          throw new IllegalArgumentException();
+        }
+      } else {
+        formValue = request.getParameter(formKey);
+      }
+      setOrganizationProperty(propertyKey, formValue);
+    }
+  }
+
+  private String getParameterOrThrow(HttpServletRequest request, String formKey) {
+    String result = request.getParameter(formKey);
+    if(result.isEmpty() || result == null) {
+        throw new IllegalArgumentException("Form value cannot be null");
+    }
+    return result;
+  }
+
+  private void setOrganizationProperty(String propertyKey, String formValue) {
+    if(propertyKey.equals("moderatorList")) {
+      ArrayList<String> newModeratorList = new ArrayList<String>(Arrays.asList(formValue.split("\\s*,\\s*")));
+      this.entity.setProperty("moderatorList", newModeratorList);
+    } else if(propertyKey.equals("isApproved")) {
+      if(formValue.equals("approved")) {
+        this.entity.setProperty("isApproved", true);
+      } else if(formValue.equals("notApproved")) {
+          this.entity.setProperty("isApproved", false);
+        }
+    } else {
+        this.entity.setProperty(propertyKey, formValue);
+    }
+  }
+}
