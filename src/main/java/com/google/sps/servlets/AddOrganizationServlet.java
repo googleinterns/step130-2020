526e8c255a0c70b969deb02732de925d3bba33aa
Sarah-Addo <36671046+Sarah-Addo@users.noreply.github.com>
2020-07-13 10:11:49 -0400

    Update AddOrganizationServlet.java

--- src/main/java/com/google/sps/servlets/AddOrganizationServlet.java
+++ src/main/java/com/google/sps/servlets/AddOrganizationServlet.java
@@ -72,11 +72,10 @@ public class AddOrganizationServlet extends HttpServlet {
     newOrganizationEntity.setProperty("changeHistory", changeHistory);
 
     OrganizationUpdater organizationUpdater = new OrganizationUpdater(newOrganizationEntity);
-    long organizationId = newOrganizationEntity.getKey().getId();
     
     // update rest of organization properties from inputted form
     try {
-      organizationUpdater.updateOrganization(request, organizationId, user, /*forRegistration*/ true);
+      organizationUpdater.updateOrganization(request, user, /*forRegistration*/ true);
     } catch(IllegalArgumentException err) {
         response.sendError(HttpServletResponse.SC_NOT_FOUND);
         return;
