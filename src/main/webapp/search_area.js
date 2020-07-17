6ea167c17d6d547292b7c0843a2968d02c0b6517
andrewm898 <54298330+andrewm898@users.noreply.github.com>
2020-07-17 15:12:19 -0400

    fix weird spacing

--- src/main/webapp/search_area.js
+++ src/main/webapp/search_area.js
@@ -11,13 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
- 
+
 document.addEventListener("DOMContentLoaded", async function() {
   // TODO: Get Maintainer status by checking if requester User is a Maintainer. 
   // This checks that requester User has valid credentials to edit/delete/view ALL organizations. (by checking userID)
   let isMaintainer = false;
   let forOrganizationsPage = false;
- 
+
   // TODO: Create separate JS file for listing organizations, instead of using SearchArea and having these checks below
   if (document.getElementById('search-area')) {
     const mainSearchArea = new SearchArea(document.getElementById('search-area'), isMaintainer, forOrganizationsPage);
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", async function() {
     organizationSearchArea.renderListOfOrganizations();
   }
 });
- 
+
 class SearchArea {
   constructor(searchAreaElement, isMaintainer, forOrganizationsPage) {
     this.searchArea = searchAreaElement;
@@ -41,51 +41,51 @@ class SearchArea {
     this.forOrganizationsPage = forOrganizationsPage;
     this.filterParams = new URLSearchParams();
     this.organizationObjectsList = [];
- 
+
     this.zipcodeFormArea = document.createElement("div");
     this.form = document.createElement("form");
     this.form.setAttribute("onsubmit", "return false");
- 
+
     this.zipcodeFormLabel = document.createElement("label");
     this.zipcodeFormLabel.setAttribute("for", "zipcode-entry");
     this.zipcodeFormLabel.setAttribute("id", "zipcode-label");
     this.zipcodeFormLabel.textContent = "Enter a zipcode: ";
     this.form.appendChild(this.zipcodeFormLabel);
- 
+
     this.zipcodeFormEntry = document.createElement("input");
     this.zipcodeFormEntry.setAttribute("type", "text");
     this.zipcodeFormEntry.setAttribute("id", "zipcode-entry");
     this.zipcodeFormEntry.setAttribute("pattern", "[0-9]{5}");
     this.zipcodeFormEntry.setAttribute("name", "zipcode");
     this.form.appendChild(this.zipcodeFormEntry);
- 
+
     this.zipcodeSubmit = document.createElement("input");
     this.zipcodeSubmit.setAttribute("type", "submit");
     this.zipcodeSubmit.setAttribute("class", "gray-button");
     this.zipcodeSubmit.addEventListener('click', () => this.setUrlParamValue("zipcode", this.form.zipcode.value));
     this.form.appendChild(this.zipcodeSubmit);
- 
+
     this.zipcodeFormArea.appendChild(this.form);
     this.organizationSearchArea.appendChild(this.zipcodeFormArea);
- 
+
     this.filterTagArea = new FilterTagArea(this);
- 
+
     this.organizationListArea = document.createElement("div");
     this.organizationListArea.setAttribute("id", "organization-list");
-    
+   
     this.organizationPopupArea = document.createElement("div");
     this.organizationPopupArea.setAttribute("id", "organization-popup-area");
     this.organizationPopupArea.classList.add("hide-popup");
- 
+
     this.organizationSearchArea.appendChild(this.organizationListArea);
     this.searchArea.appendChild(this.organizationSearchArea);
     this.searchArea.appendChild(this.organizationPopupArea);
   }
- 
+
   renderListOfOrganizations() {
     this.organizationObjectsList.forEach((organization) => {
       const newOrganization = new Organization(organization, this.isMaintainer, this.forOrganizationsPage);
- 
+
       newOrganization.organizationElement.addEventListener('organization-selected', () => {
         const organizationPopupArea = document.getElementById("organization-popup-area");
         organizationPopupArea.textContent = "";
@@ -93,14 +93,14 @@ class SearchArea {
         organizationPopupArea.classList.add("show-popup");
         organizationPopupArea.classList.remove("hide-popup");
       });
- 
+
       newOrganization.closeButtonElement.addEventListener('organization-close', () => {
       //  Remove the popup from the DOM.
         document.getElementById("organization-popup-area").classList.add("hide-popup");
         document.getElementById("organization-popup-area").classList.remove("show-popup");
         newOrganization.popupElement.remove();
       });
- 
+
       this.organizationListArea.appendChild(newOrganization.getOrganization());
     });
     if (this.organizationObjectsList.length === 0) {
@@ -110,7 +110,7 @@ class SearchArea {
       this.organizationListArea.appendChild(noResultsFoundMessage);
     }
   }
- 
+
   async getListOfOrganizations() {
     let response;
     if (this.filterParams) {
@@ -120,14 +120,14 @@ class SearchArea {
     }
     this.organizationObjectsList = await response.json();
   }
- 
+
   async setUrlParamValue(urlParamKey, urlParamValue) {
     /* New query value is not added if it is a duplicate or empty/null */
     if (this.filterParams.getAll(urlParamKey).includes(urlParamValue) ||
         (urlParamValue === null) || (urlParamValue.trim() === "")) {
       return;
     }
-    
+
     /* if the param is a zipcode, remove tag of any existing one & set new one*/ 
     if (urlParamKey === "zipcode") {
       if (this.filterParams.get("zipcode")) {
@@ -146,9 +146,9 @@ class SearchArea {
     this.renderListOfOrganizations();
   }
 }
- 
+
 class FilterTagArea {
- 
+
   constructor(searchArea) {
     this.parentSearchArea = searchArea;
 
@@ -237,12 +237,12 @@ class FilterTagArea {
     if (urlParamKey === "zipcode") {
       filterTagArea.setAttribute("id", "zipcodeTag");
     }
- 
+
     let filterTagLabel = document.createElement("div");
     filterTagLabel.textContent = urlParamValue;
     filterTagLabel.setAttribute("class", "filter-tag-label");
     filterTagArea.appendChild(filterTagLabel);
- 
+
     let filterTagClose = document.createElement("div");
     filterTagClose.addEventListener('click', () => this.removeFilterTag(urlParamKey, urlParamValue, filterTagArea));
     filterTagClose.textContent = 'X';
