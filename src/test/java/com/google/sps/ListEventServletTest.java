2579f18cd39fd56ec1f464ddb7331c123fd8beb6
andrewm898 <54298330+andrewm898@users.noreply.github.com>
2020-07-28 14:15:04 -0400

    add test for filtering

--- src/test/java/com/google/sps/ListEventServletTest.java
+++ src/test/java/com/google/sps/ListEventServletTest.java
@@ -58,65 +58,72 @@ public final class ListEventServletTest {
   @Before
   public void setUp() {
     helper.setUp();
-    //                  Existing entity table
-    // +-------+-------------------------+------------------+---------+
-    // | Index | creationTimeStampMillis | eventOwnerOrgIds | zipCode |
-    // +-------+-------------------------+------------------+---------+
-    // |     0 |                       0 | 1, 3, 7          |   12345 |
-    // |     1 |                       1 | 3, 9, 4          |   02763 |
-    // |     2 |                       2 | 0, 2, 5          |   47906 |
-    // |     3 |                       3 | 4                |   47906 |
-    // |     4 |                       4 | 5, 8, 0          |   02763 |
-    // |     5 |                       5 | 1, 4             |   02763 |
-    // |     6 |                       6 | 6, 3             |   94566 |
-    // +-------+-------------------------+------------------+---------+
+    //                                Existing entity table
+    // +-------+-------------------------+------------------+---------+---------------+
+    // | Index | creationTimeStampMillis | eventOwnerOrgId  | zipCode | streetAddress |
+    // +-------+-------------------------+------------------+---------+---------------+
+    // |     0 |                       0 | 1                |   12345 | 12 Oak st.    |
+    // |     1 |                       1 | 3                |   02763 | 12 Oak st.    |
+    // |     2 |                       2 | 0                |   47906 | 10 Main st.   |
+    // |     3 |                       3 | 4                |   02763 | 123 Park ln.  |
+    // |     4 |                       4 | 5                |   02763 | 10 Main st.   |
+    // |     5 |                       5 | 1                |   02763 | 14 Oak st.    |
+    // |     6 |                       6 | 6                |   94566 | 100 Birch st. |
+    // +-------+-------------------------+------------------+---------+---------------+
 
     Entity entity0 = new Entity("Event");
     entity0.setProperty("creationTimeStampMillis", 0);
-    entity0.setProperty("eventOwnerOrgIds", new ArrayList<Long>(Arrays.asList(new Long(1), new Long(3), new Long(7))));
+    entity0.setProperty("eventOwnerOrgId", new Long(1));
     entity0.setProperty("eventZipCode", "12345");
+    entity0.setProperty("eventStreetAddress", "12 Oak st.");
     masterEntityList.add(entity0);
     datastore.put(entity0);
  
     Entity entity1 = new Entity("Event");
     entity1.setProperty("creationTimeStampMillis", 1);
-    entity1.setProperty("eventOwnerOrgIds", new ArrayList<Long>(Arrays.asList(new Long(3), new Long(9), new Long(4))));
+    entity1.setProperty("eventOwnerOrgId", new Long(3));
     entity1.setProperty("eventZipCode", "02763");
+    entity1.setProperty("eventStreetAddress", "12 Oak st.");
     masterEntityList.add(entity1);
     datastore.put(entity1);
  
     Entity entity2 = new Entity("Event");
     entity2.setProperty("creationTimeStampMillis", 2);
-    entity2.setProperty("eventOwnerOrgIds", new ArrayList<Long>(Arrays.asList(new Long(0), new Long(2), new Long(5))));
+    entity2.setProperty("eventOwnerOrgId", new Long(0));
     entity2.setProperty("eventZipCode", "47906");
+    entity2.setProperty("eventStreetAddress", "10 Main st.");
     masterEntityList.add(entity2);
     datastore.put(entity2);
  
     Entity entity3 = new Entity("Event");
     entity3.setProperty("creationTimeStampMillis", 3);
-    entity3.setProperty("eventOwnerOrgIds", new ArrayList<Long>(Arrays.asList(new Long(4))));
-    entity3.setProperty("eventZipCode", "47906");
+    entity3.setProperty("eventOwnerOrgId", new Long(4));
+    entity3.setProperty("eventZipCode", "02763");
+    entity3.setProperty("eventStreetAddress", "123 Park ln.");
     masterEntityList.add(entity3);
     datastore.put(entity3);
  
     Entity entity4 = new Entity("Event");
     entity4.setProperty("creationTimeStampMillis", 4);
-    entity4.setProperty("eventOwnerOrgIds", new ArrayList<Long>(Arrays.asList(new Long(5), new Long(8), new Long(0))));
+    entity4.setProperty("eventOwnerOrgId", new Long(5));
     entity4.setProperty("eventZipCode", "02763");
+    entity4.setProperty("eventStreetAddress", "10 Main st.");
     masterEntityList.add(entity4);
     datastore.put(entity4);
  
     Entity entity5 = new Entity("Event");
     entity5.setProperty("creationTimeStampMillis", 5);
-    entity5.setProperty("eventOwnerOrgIds", new ArrayList<Long>(Arrays.asList(new Long(1), new Long(4))));
+    entity5.setProperty("eventOwnerOrgId", new Long(1));
     entity5.setProperty("eventZipCode", "02763");
+    entity5.setProperty("eventStreetAddress", "14 Oak st.");
     masterEntityList.add(entity5);
     datastore.put(entity5);
  
     Entity entity6 = new Entity("Event");
     entity6.setProperty("creationTimeStampMillis", 6);
-    entity6.setProperty("eventOwnerOrgIds", new ArrayList<Long>(Arrays.asList(new Long(6), new Long(3))));
+    entity6.setProperty("eventOwnerOrgId", new Long(6));
     entity6.setProperty("eventZipCode", "94566");
+    entity6.setProperty("eventStreetAddress", "100 Birch st.");
     masterEntityList.add(entity6);
     datastore.put(entity6);
   }
@@ -128,6 +135,7 @@ public final class ListEventServletTest {
 
   @Test
   public void testEventQueryForUserOrgs() {
+    /* Tests to make sure that client can successfully request only user's events */
     GivrUser mockUser = mock(GivrUser.class);
     
     ArrayList<Entity> moderatingOrgs = new ArrayList<Entity>();
@@ -135,20 +143,45 @@ public final class ListEventServletTest {
     moderatingOrgs.add(new Entity("Distributor", 1));
     moderatingOrgs.add(new Entity("Distributor", 3));
     when(mockUser.getModeratingOrgs()).thenReturn(moderatingOrgs);
-    
-    /* Expected list only contains the events hosted by orgs 1 & 3 */
+
+    HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+    when(mockRequest.getParameter("displayForUser")).thenReturn("true");
+
+    /* Expected list only contains the events hosted by orgs 1 or 3 */
     ArrayList<Entity> expectedList = new ArrayList<Entity>();
-    expectedList.add(masterEntityList.get(6));
     expectedList.add(masterEntityList.get(5));
     expectedList.add(masterEntityList.get(1));
     expectedList.add(masterEntityList.get(0));
-
-    HttpServletRequest mockRequest = mock(HttpServletRequest.class);
-    when(mockRequest.getParameter("zipcode")).thenReturn(null);
-    when(mockRequest.getParameter("displayForUser")).thenReturn("true");
  
     Query receivedQuery = ListHelper.getQuery("Event", mockRequest, mockUser);
  
+    FetchOptions fetchOptions = FetchOptions.Builder.withLimit(10);
+    Assert.assertArrayEquals(expectedList.toArray(), datastore.prepare(receivedQuery).asList(fetchOptions).toArray());
+  }
+
+  @Test
+  public void testEventQueryForFilteredUserOrgs() {
+    /* Tests to make sure client can successfully see user's events with filters */
+    GivrUser mockUser = mock(GivrUser.class);
+    
+    ArrayList<Entity> moderatingOrgs = new ArrayList<Entity>();
+    /* This user moderates the orgs w/ ID's 4, 5, and 6 */
+    moderatingOrgs.add(new Entity("Distributor", 4));
+    moderatingOrgs.add(new Entity("Distributor", 5));
+    moderatingOrgs.add(new Entity("Distributor", 6));
+    when(mockUser.getModeratingOrgs()).thenReturn(moderatingOrgs);
+
+    HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+    when(mockRequest.getParameterValues("zipcode")).thenReturn(new String[]{"02763"});
+    when(mockRequest.getParameterValues("streetAddress")).thenReturn(new String[]{"10 Main st."});
+    when(mockRequest.getParameter("displayForUser")).thenReturn("true");
+
+    /* Expected list only contains the events hosted by orgs 4, 5, or 6 in zipcode 02763 w/ address 10 Main st. */
+    ArrayList<Entity> expectedList = new ArrayList<Entity>();
+    expectedList.add(masterEntityList.get(4));
+ 
+    Query receivedQuery = ListHelper.getQuery("Event", mockRequest, mockUser);
+
     FetchOptions fetchOptions = FetchOptions.Builder.withLimit(10);
     Assert.assertArrayEquals(expectedList.toArray(), datastore.prepare(receivedQuery).asList(fetchOptions).toArray());
   }
