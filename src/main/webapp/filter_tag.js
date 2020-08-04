9da34da28723fdc63f7c069858da7095f0013147
andrewm898 <54298330+andrewm898@users.noreply.github.com>
2020-08-04 10:46:15 -0400

    Update filter_tag.js

--- src/main/webapp/filter_tag.js
+++ src/main/webapp/filter_tag.js
@@ -33,7 +33,7 @@ class FilterTag {
     this.filterTagClose = document.createElement("div");
     this.filterTagClose.textContent = 'X';
     this.filterTagClose.setAttribute("class", "filter-tag-close");
-    this.filterTagClose.addEventListener('click', () => this.parentFilterTagArea.removeFilterTag(urlParamKey, urlParamValue, this, true));
+    this.filterTagClose.addEventListener('click', () => this.parentFilterTagArea.removeFilterTag(urlParamKey, urlParamValue, this, /* refreshObjects= */ true));
     this.filterTagArea.appendChild(this.filterTagClose);
   }
 }
